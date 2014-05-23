7487529ac0a25f3fc15623e52198de7957a81be6
Maaiins <Nicolai.Lauser@web.de>
2014-05-23 12:05:32 +0200

    MARC HELP

--- src/tinf13b4/forum/register/ValidateRegister.java
+++ src/tinf13b4/forum/register/ValidateRegister.java
@@ -15,9 +15,12 @@ public class ValidateRegister {
 	
 	private String error;
 	
-	private static final String emailpattern = 
-			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private Pattern pattern;
+	private Matcher matcher;
+ 
+	private static final String EMAIL_PATTERN = 
+		"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 	
 	public String getConfirmpassword() {
 		return confirmpassword;
@@ -47,22 +50,22 @@ public class ValidateRegister {
 		return error;
 	}
 
-		 
+		  
 	public EmailValidator() {
-		email = email.compile(emailpattern);
+		pattern = Pattern.compile(EMAIL_PATTERN);
 	}
-	 
+ 
 	/**
 	 * Validate hex with regular expression
 	 * 
 	 * @param hex
-	 * hex for validation
+	 *            hex for validation
 	 * @return true valid hex, false invalid hex
 	 */
 	public boolean validate(final String hex) {
-	 
-		matcher = email.matcher(hex);
+ 
+		matcher = pattern.matcher(hex);
 		return matcher.matches();
-	 
+ 
 	}
 }
