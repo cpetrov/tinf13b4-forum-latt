3ba5131a9111a24daf375cc67dbce113dbff79fa
xrayf <stoppel13@msn.com>
2014-05-26 15:35:18 +0200

    Adds an Installer to initialize the database tables.
    Change-Id: I520a9c7e8442aeeff1c2d68b28ee81c01f9a91d1

---
+++ src/main/java/tinf13b4/forum/database/ConnectionFactory.java
@@ -0,0 +1,43 @@
+
+package tinf13b4.forum.database;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.List;
+
+import tinf13b4.forum.util.Base64Decryptor;
+import tinf13b4.forum.util.ConnectionUtil;
+import tinf13b4.forum.util.StringUtil;
+
+import com.google.common.base.Preconditions;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class ConnectionFactory {
+
+	private File dbConfig;
+
+	public ConnectionFactory(File dbConfig) {
+		Preconditions.checkArgument(dbConfig != null, "DbConfig must not be null.");
+		this.dbConfig = dbConfig;
+	}
+
+	public Connection createConnection() {
+		List<String> dataList = readDataListFromFile();
+		MysqlDataSource dataSource = ConnectionUtil.createDataSource(dataList);
+		return ConnectionUtil.openConnection(dataSource);
+	}
+
+	private List<String> readDataListFromFile() {
+		InputStream inputStream;
+		try {
+			inputStream = new FileInputStream(dbConfig);
+		} catch (FileNotFoundException e) {
+			throw new IllegalStateException("File not found.", e);
+		}
+		List<String> encryptedDataList = StringUtil.getStringListFromInputStream(inputStream);
+		return Base64Decryptor.getDecryptedStringList(encryptedDataList);
+	}
+}
