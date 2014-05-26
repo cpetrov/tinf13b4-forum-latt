e2a7a73aec927731eea5b37ddf03fe44845a4439
xrayf <stoppel13@msn.com>
2014-05-26 20:42:15 +0200

    ConnectionManager Review worked in. TODO added. executeSqlScript added: Uses ScriptRunner to execute SQL-Script

--- src/tinf13b4/forum/database/ConnectionManager.java
+++ src/tinf13b4/forum/database/ConnectionManager.java
@@ -1,77 +1,83 @@
 package tinf13b4.forum.database;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
 import tinf13b4.forum.security.DecryptBase64;
+
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
-public class ConnectionManager 
-{
+public class ConnectionManager {
 	private ArrayList<String> data;
 
 	private Connection conn;
 	private Statement stat;
-//	public Connection getConnection() {
-//		openConnection();
-//		return connection;
-//	}
-	
-	public ConnectionManager() 
-	{
-//		data = new DecryptBase64().DecryptBase64(System.getProperty("java.class.path"));
+
+	public static void main(String[] args) {
+		new ConnectionManager();
 	}
-	
-	public ConnectionManager(String filepath)
-	{
-		data = new DecryptBase64().DecryptBase64(filepath);
+
+	public ConnectionManager() {
+		// TODO relative path zu DBConfig.cfg
+		String path = "/tinf13b4-forum-lat/src/tinf13b4/forum/databaseConfigurator/DBConfig.cfg";
+		data = new DecryptBase64().DecryptBase64(path);
 	}
-	
-	private Connection openConnection()
-	{
+
+	private void openConnection() {
 		try {
-	//		Context context = new InitialContext();
 			MysqlDataSource dataSource = new MysqlDataSource();
 			dataSource.setServerName(data.get(0));
 			dataSource.setPort(Integer.parseInt(data.get(1)));
 			dataSource.setDatabaseName(data.get(2));
 			dataSource.setUser(data.get(3));
 			dataSource.setPassword(data.get(4));
-			
-			
+
 			conn = dataSource.getConnection();
 			stat = conn.createStatement();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return conn;
 	}
-	
-	private void closeConnection()
-	{
+
+	private void closeConnection() {
 		try {
 			stat.close();
 			conn.close();
-		} catch (SQLException e) 
-		{
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public ResultSet executeCommand(String command) {
 		openConnection();
 		ResultSet rs = null;
 		try {
 			rs = stat.executeQuery(command);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		closeConnection();
 		return rs;
+	}
+
+	public void executeSqlScript(String filename) {
+		ScriptRunner runner = new ScriptRunner(conn, false, false);
+		try {
+			runner.runScript(new BufferedReader(new FileReader(filename)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
