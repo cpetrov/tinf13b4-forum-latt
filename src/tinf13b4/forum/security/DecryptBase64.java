83244934508288fc95bff716a3f5231f4c14078e
xrayf <stoppel13@msn.com>
2014-05-26 20:37:15 +0200

    DecryptBase64 Review Changes worked into

--- src/tinf13b4/forum/security/DecryptBase64.java
+++ src/tinf13b4/forum/security/DecryptBase64.java
@@ -8,43 +8,36 @@ import java.util.ArrayList;
 import javax.xml.bind.DatatypeConverter;
 
 public class DecryptBase64 
-{
+ {
 	private ArrayList<String> data;
-	
-	public DecryptBase64() {
-		// TODO Auto-generated constructor stub
-	}
 
-	public ArrayList<String> DecryptBase64(final String filepath) 
-	{	
+/* Return DATA
+ * Position 0: Hostname
+ * Position 1: Port
+ * Position 2: Databasename
+ * Position 3: Username
+ * Position 4: Password
+ */	
+	public ArrayList<String> DecryptBase64(final String filepath) {
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File(filepath)));
+			BufferedReader reader = new BufferedReader(new FileReader(new File(
+					filepath)));
 			data = new ArrayList<String>();
-			while(reader.ready())
-			{
+			while (reader.ready()) {
 				data.add(reader.readLine());
 			}
-		} catch (Exception e) 
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		int i = 0;
-		for(String a : data)
-		{
+		for (String a : data) {
 			data.set(i, decrypt(a));
 			i++;
 		}
-		/*
-		 * Position 0: Hostname
-		 * Position 1: Port
-		 * Position 2: Databasename
-		 * Position 3: Username
-		 * Position 4: Password
-		 */
 		return data;
 	}
 	
-	public String decrypt(String s){
-        return new String(DatatypeConverter.parseBase64Binary(s));
+	public String decrypt(String s) {
+		return new String(DatatypeConverter.parseBase64Binary(s));
 	}
 }
