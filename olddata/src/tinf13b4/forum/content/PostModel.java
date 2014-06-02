a3d247c7b6601d98c6d5d8e0f8375ebcbf7b03ce
Maaiins <Nicolai.Lauser@web.de>
2014-06-02 21:10:05 +0200

    Added old data
    Change-Id: If185af1485d9d9689dfc4b48c7815e4b85e76b8f

---
+++ olddata/src/tinf13b4/forum/content/PostModel.java
@@ -0,0 +1,48 @@
+package tinf13b4.forum.content;
+
+import java.util.Date;
+
+public class PostModel {
+	private int post_ID;
+	private String content;
+	private String poster;
+	private Date date;
+	private String thread_ID;
+	
+	public PostModel(int post_ID) {
+		this.post_ID = post_ID;
+	}
+	
+	public int getPost_ID() {
+		return post_ID;
+	}
+	public void setPost_ID(int post_ID) {
+		this.post_ID = post_ID;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getThread_ID() {
+		return thread_ID;
+	}
+	public void setThread_ID(String thread_ID) {
+		this.thread_ID = thread_ID;
+	}
+	
+
+}
