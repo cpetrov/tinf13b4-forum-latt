494edc10733883e5df488e831d679bd265f42e85
xrayf <stoppel13@msn.com>
2014-06-02 16:11:29 +0200

    Database-Configurator Program with GUI to create a "DBConfig.cfg" file with encrypted database connection information in Base64.
    Change-Id: I287d118efee9224a9632ad070b1b7fe5c7058304

---
+++ src/main/java/tinf13b4/forum/databaseConfigurator/ViewModel.java
@@ -0,0 +1,62 @@
+
+package tinf13b4.forum.databaseConfigurator;
+
+import java.nio.file.Path;
+
+public class ViewModel {
+
+	private String hostname;
+	private int port;
+	private String databasename;
+	private String username;
+	private String password;
+	private Path path;
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getDatabasename() {
+		return databasename;
+	}
+
+	public void setDatabasename(String databasename) {
+		this.databasename = databasename;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Path getPath() {
+		return path;
+	}
+
+	public void setPath(Path path) {
+		this.path = path;
+	}
+}
