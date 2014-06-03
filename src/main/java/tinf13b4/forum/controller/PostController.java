456f8c6d477bba9fbf6cd74bf0a0a8388041863b
xrayf <stoppel13@msn.com>
2014-06-03 16:01:43 +0200

    Post & UserController added

---
+++ src/main/java/tinf13b4/forum/controller/PostController.java
@@ -0,0 +1,61 @@
+package tinf13b4.forum.controller;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import tinf13b4.forum.database.ConnectionFactory;
+import tinf13b4.forum.database.QueryExecutor;
+import tinf13b4.forum.model.Post;
+import tinf13b4.forum.model.PostBuilder;
+
+public class PostController {
+
+	private QueryExecutor executor;
+	private ArrayList<Post> posts;
+	private ResultSet rs;
+
+	public PostController() {
+		ConnectionFactory factory = new ConnectionFactory(new File(System.getProperty("user.home") + "/DBConfig.cfg"));
+		executor = new QueryExecutor(factory.createConnection());
+	}
+
+	protected PostController(QueryExecutor executor) {
+		this.executor = executor;
+	}
+	
+	public ArrayList<Post> getPosts(int threadId) {
+		posts = new ArrayList<Post>();
+		checkArgument(threadId>=0, "ThreadID must be >= 0, but was " + threadId);
+		rs = executor.executeQuery("SELECT P.Post_ID, P.Content, P.Date, P.User_ID"
+				+ " FROM Posts P, Threads T, ThreadPostRelationTable TP "
+				+ "WHERE P.Post_ID = TP.Post_ID "
+				+ "AND TP.Thread_ID = T.Thread_ID "
+				+ "AND T.Thread_ID = " + threadId + ";");
+		if (rs == null)
+			return posts;
+		else {
+			try {
+				while (rs.next()) {
+					posts.add(buildPost());
+				}
+			} catch (SQLException e) {
+				new IllegalStateException("SQL Error: " + e);
+			}
+		}
+		return posts;
+	}
+	
+	
+	private Post buildPost() throws SQLException {
+		PostBuilder postBuilder = new PostBuilder();
+		postBuilder.setPostId(rs.getInt("Post_ID"));
+		postBuilder.setUserId(rs.getInt("User_ID"));
+		postBuilder.setContent(rs.getString("Content"));
+		postBuilder.setDate(rs.getDate("Date"));
+		return postBuilder.build();
+	}
+}
