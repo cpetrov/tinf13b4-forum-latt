c7b98f42fcc095f5b6b8994e47e746f1693a8578
xrayf <stoppel13@msn.com>
2014-06-03 16:20:49 +0200

    Added Row to Users

--- src/main/resources/tinf13b4/forum/install/InitializeTables.sql
+++ src/main/resources/tinf13b4/forum/install/InitializeTables.sql
@@ -10,23 +10,10 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Exportiere Struktur von Tabelle pmforum.Users
-CREATE TABLE IF NOT EXISTS `Users` (
-  `User_ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `Name` varchar(64) NOT NULL DEFAULT '0',
-  `EMail` varchar(254) NOT NULL DEFAULT '0',
-  `Password` varchar(60) NOT NULL DEFAULT '0',
-  `Confirmed` tinyint(4) unsigned NOT NULL DEFAULT '0',
-  `Confirmation Key` varchar(36) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`User_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for Users';
-
--- Daten Export vom Benutzer nicht ausgewählt
-
 -- Exportiere Struktur von Tabelle pmforum.Category
 CREATE TABLE IF NOT EXISTS `Category` (
-  `Category_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` text NOT NULL,
+  `Category_ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Title` text NOT NULL,
   `Subtitle` text NOT NULL,
   PRIMARY KEY (`Category_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for Category';
@@ -36,12 +23,14 @@ CREATE TABLE IF NOT EXISTS `Category` (
 
 -- Exportiere Struktur von Tabelle pmforum.Posts
 CREATE TABLE IF NOT EXISTS `Posts` (
-  `Post_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Content` text,
-  `Poster_ID` int(11) DEFAULT NULL,
-  `Date` date DEFAULT NULL,
+  `Post_ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Content` text NOT NULL,
+  `Date` date NOT NULL,
+  `User_ID` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`Post_ID`),
-  UNIQUE KEY `Post_ID` (`Post_ID`)
+  UNIQUE KEY `Post_ID` (`Post_ID`),
+  KEY `FK_Posts_Users` (`User_ID`),
+  CONSTRAINT `FK_Posts_Users` FOREIGN KEY (`User_ID`) REFERENCES `Users` (`User_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for Posts';
 
 -- Daten Export vom Benutzer nicht ausgewählt
@@ -49,14 +38,14 @@ CREATE TABLE IF NOT EXISTS `Posts` (
 
 -- Exportiere Struktur von Tabelle pmforum.ThreadPostRelationTable
 CREATE TABLE IF NOT EXISTS `ThreadPostRelationTable` (
-  `ThreadPost_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Thread_ID` int(10) DEFAULT NULL,
-  `Post_ID` int(11) DEFAULT NULL,
+  `ThreadPost_ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Thread_ID` int(10) unsigned NOT NULL,
+  `Post_ID` int(11) unsigned NOT NULL,
   PRIMARY KEY (`ThreadPost_ID`),
   KEY `FK_ThreadPostRelationTable_Threads` (`Thread_ID`),
   KEY `FK_ThreadPostRelationTable_Posts` (`Post_ID`),
-  CONSTRAINT `FK_ThreadPostRelationTable_Posts` FOREIGN KEY (`Post_ID`) REFERENCES `Posts` (`Post_ID`),
-  CONSTRAINT `FK_ThreadPostRelationTable_Threads` FOREIGN KEY (`Thread_ID`) REFERENCES `Threads` (`Thread_ID`)
+  CONSTRAINT `Post_ID` FOREIGN KEY (`Post_ID`) REFERENCES `Posts` (`Post_ID`),
+  CONSTRAINT `Thread_ID` FOREIGN KEY (`Thread_ID`) REFERENCES `Threads` (`Thread_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Relational Table';
 
 -- Daten Export vom Benutzer nicht ausgewählt
@@ -64,18 +53,36 @@ CREATE TABLE IF NOT EXISTS `ThreadPostRelationTable` (
 
 -- Exportiere Struktur von Tabelle pmforum.Threads
 CREATE TABLE IF NOT EXISTS `Threads` (
-  `Thread_ID` int(10) NOT NULL AUTO_INCREMENT,
-  `Name` text,
-  `Content` text,
-  `Thread Starter` int(10) unsigned DEFAULT NULL,
-  `Creation Date` date DEFAULT NULL,
-  `ReadOnly` tinyint(3) unsigned DEFAULT NULL,
-  `Category_ID` int(10) NOT NULL DEFAULT '0',
+  `Thread_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Title` text NOT NULL,
+  `Content` text NOT NULL,
+  `Date` date NOT NULL,
+  `ReadOnly` tinyint(3) unsigned NOT NULL,
+  `User_ID` int(10) unsigned NOT NULL,
+  `Category_ID` int(10) unsigned NOT NULL,
   PRIMARY KEY (`Thread_ID`),
   UNIQUE KEY `Thread_ID` (`Thread_ID`),
   KEY `FK_Threads_Category` (`Category_ID`),
-  CONSTRAINT `FK_Threads_Category` FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`Category_ID`)
+  KEY `User_ID` (`User_ID`),
+  CONSTRAINT `Category_ID` FOREIGN KEY (`Category_ID`) REFERENCES `Category` (`Category_ID`),
+  CONSTRAINT `User_ID` FOREIGN KEY (`User_ID`) REFERENCES `Users` (`User_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for Threads';
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle pmforum.Users
+CREATE TABLE IF NOT EXISTS `Users` (
+  `User_ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(64) NOT NULL DEFAULT '0',
+  `Picture` blob,
+  `Email` varchar(254) NOT NULL DEFAULT '0',
+  `Password` varchar(60) NOT NULL DEFAULT '0',
+  `JoinedOn` date NOT NULL,
+  `Confirmed` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  `Confirmation_Key` varchar(36) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`User_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for Users';
 
 -- Daten Export vom Benutzer nicht ausgewählt
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
