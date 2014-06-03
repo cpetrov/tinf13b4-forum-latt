2d3393a6589d63b25358bdb21c5bad81c7246eea
xrayf <stoppel13@msn.com>
2014-06-03 18:00:58 +0200

    Category.jsp and DummyProviderBean.java fixed

--- src/main/java/tinf13b4/forum/beans/DummyProviderBean.java
+++ src/main/java/tinf13b4/forum/beans/DummyProviderBean.java
@@ -19,11 +19,12 @@ public class DummyProviderBean {
 
 	private List<Category> categories;
 	private List<Thread> threads;
-	private List<User> members;
+	private List<User> users;
 	private List<Post> posts;
 	private String longText;
 	private String heading;
 	private int id;
+	private Category category;
 
 	public DummyProviderBean() {
 		createLongText();
@@ -63,10 +64,10 @@ public class DummyProviderBean {
 		posts = controller.getPosts(id);
 	}
 
-	private void createMembers() {
-		members = new ArrayList<>();
+	private void createUsers() {
+		users = new ArrayList<>();
 		UserController controller = new UserController();
-		members = controller.getUsers();
+		users = controller.getUsers();
 	}
 
 	private void createThreads() {
@@ -92,8 +93,8 @@ public class DummyProviderBean {
 	}
 
 	public List<User> getMembers() {
-		createMembers();
-		return members;
+		createUsers();
+		return users;
 	}
 
 	public List<Post> getPosts() {
@@ -111,5 +112,21 @@ public class DummyProviderBean {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public Category getCategory() {
+		setCategory(category);
+		return category;
+	}
+	
+	public void setCategory(Category category) {
+		if(categories == null)
+			createCategories();
+
+		for (Category a : categories)
+			if (a.getId() == id) {
+				this.category = a;
+				return;
+			}
 	}
 }
