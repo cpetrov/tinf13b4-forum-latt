882fab754fcae89efe5b3eab0ad826a294ba95a1
xrayf <stoppel13@msn.com>
2014-06-03 16:02:00 +0200

    CategoryBuilder & UserBuilder added

---
+++ src/main/java/tinf13b4/forum/model/UserBuilder.java
@@ -0,0 +1,43 @@
+
+package tinf13b4.forum.model;
+
+import java.sql.Blob;
+import java.util.Date;
+
+public class UserBuilder {
+
+	private int id;
+	private String name;
+	private Blob picture;
+	private String eMail;
+	private Date joinedOn;
+
+	public UserBuilder setId(int id) {
+		this.id = id;
+		return this;
+	}
+
+	public UserBuilder setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public UserBuilder setPicture(Blob picture) {
+		this.picture = picture;
+		return this;
+	}
+
+	public UserBuilder setEMail(String eMail) {
+		this.eMail = eMail;
+		return this;
+	}
+	
+	public UserBuilder setJoinedOn(Date joinedOn) {
+		this.joinedOn = joinedOn;
+		return this;
+	}
+
+	public User build() {
+		return new User(id, name, picture, eMail, joinedOn);
+	}
+}
