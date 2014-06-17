0c30f96cfbaf58657b79b907f92e7208640380fc
xrayf <stoppel13@msn.com>
2014-06-17 14:08:01 +0200

    Fixed Service Mode Added new SQL Added OrderNumber Fixed EditCategories on AdminPanel

--- src/main/java/tinf13b4/forum/model/CategoryBuilder.java
+++ src/main/java/tinf13b4/forum/model/CategoryBuilder.java
@@ -5,6 +5,7 @@ public class CategoryBuilder {
 	private int id;
 	private String title;
 	private String subtitle;
+	private int orderNumber;
 
 	public CategoryBuilder setId(int id) {
 		this.id = id;
@@ -20,8 +21,13 @@ public class CategoryBuilder {
 		this.subtitle = subtitle;
 		return this;
 	}
+	
+	public CategoryBuilder setOrderNumber(int orderNumber) {
+		this.orderNumber = orderNumber;
+		return this;
+	}
 
 	public Category build() {
-		return new Category(id, title, subtitle);
+		return new Category(id, title, subtitle, orderNumber);
 	}
 }
