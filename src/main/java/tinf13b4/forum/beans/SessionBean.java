c95d138a1b4c5201b8e02f6df7aa2192792098f8
xrayf <stoppel13@msn.com>
2014-06-17 16:13:45 +0200

    session Change-Id: I4edc1f66cc30b7a160b4db9b9309326f2e3457cf

---
+++ src/main/java/tinf13b4/forum/beans/SessionBean.java
@@ -0,0 +1,40 @@
+package tinf13b4.forum.beans;
+
+import java.util.Date;
+
+public class SessionBean {
+
+	private String id;
+	private String userName;
+	private Date createTime;
+	private boolean isLoggedIn = false;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(long createTime) {
+		this.createTime = new Date(createTime);
+	}
+	
+	public void login(){
+		this.isLoggedIn = true;
+	}
+	
+	public void logout(){
+		this.isLoggedIn = false;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
