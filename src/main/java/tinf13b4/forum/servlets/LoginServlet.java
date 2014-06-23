2f57720a09c03872b08bc9b90f07a27e8f682053
xrayf <stoppel13@msn.com>
2014-06-23 12:00:27 +0200

    Session
    Change-Id: I4edc1f66cc30b7a160b4db9b9309326f2e3457cf

--- src/main/java/tinf13b4/forum/servlets/LoginServlet.java
+++ src/main/java/tinf13b4/forum/servlets/LoginServlet.java
@@ -8,11 +8,13 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.eclipse.rap.json.JsonArray;
 import org.eclipse.rap.json.JsonObject;
 import org.eclipse.rap.json.JsonValue;
 
+import tinf13b4.forum.beans.SessionBean;
 import tinf13b4.forum.controller.LoginController;
 
 @WebServlet("/api/login")
@@ -27,43 +29,51 @@ public class LoginServlet extends JsonServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response, JsonObject postData) throws ServletException, IOException {
-		
+
 		List<String> errors = new ArrayList<String>();
-		
+
 		JsonValue logonCredential = postData.get("name");
 		JsonValue password = postData.get("password");
-		
-		// 
+
+		//
 		if(logonCredential == null){
 			errors.add("Name can not be empty");
 		}
-		
+
 		// If Password is empty return error message
-		if(password == null){			
+		if(password == null){
 			errors.add("Password can not be empty");
 		}
-		
+
 		// Check error size
 		if(errors.size() > 0){
 			JsonObject json = errorListToJson(errors);
-			
+
 			json.writeTo(response.getWriter());
 		} else {
 
 			// Validate with Database
 			errors = loginController.loginDataValidator(logonCredential.asString(), password.asString());
-			
+
 			if(errors.size() > 0){
 				JsonObject json = errorListToJson(errors);
-				
+
 				json.writeTo(response.getWriter());
+			}else{
+				HttpSession session = request.getSession();
+				SessionBean userSession = (SessionBean)session.getAttribute("userSession");
+				userSession.setId(session.getId());
+				userSession.setUserName(postData.get("name").asString());
+				userSession.setCreateTime(session.getCreationTime());
+
+				userSession.login();
 			}
 		}
 	}
-	
+
 	private JsonObject errorListToJson(List<String> errors){
 		JsonArray json = new JsonArray();
 
@@ -74,7 +84,7 @@ public class LoginServlet extends JsonServlet {
 		JsonObject jsonObject = new JsonObject();
 
 		jsonObject.add("errors", json);
-		
+
 		return jsonObject;
 	}
 }
